125190f61a83499a17996978f10d3832cda3a373
unknown <ingrith08@gmail.com>
2022-02-23 16:52:18 -0500

    Delete cors

--- main.go
+++ main.go
@@ -3,9 +3,7 @@ package main
 import (
 	"log"
 	"os"
-	"strings"
 
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"test.com/mutant-go/application/db"
@@ -16,12 +14,6 @@ func main() {
 	loadEnv()
 	db.Initialize()
 	server := gin.Default()
-
-	config := cors.DefaultConfig()
-	config.AllowOrigins = strings.Split(os.Getenv("ALLOW_URL"), ";")
-	config.AllowCredentials = true
-	config.AllowHeaders = []string{"content-type", "captcha"}
-	server.Use(cors.New(config))
 
 	routes.Routes(server)
 	server.Run(os.Getenv("PORT"))
